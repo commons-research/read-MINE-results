b9832b928926911559ec0601527a83fceacead1d
Pascal Amrein <pascal.amrein@students.unibe.ch>
2024-07-21 14:30:55 +0200

    changed path for ibu

--- read_mine_results/db_files/00_run_all.sh
+++ read_mine_results/db_files/00_run_all.sh
@@ -5,7 +5,7 @@
 
 #SBATCH --partition=pibu_el8
 #SBATCH --mem=900G
-#SBATCH --time=1-10:00:00
+#SBATCH --time=3-10:00:00
 #SBATCH --job-name="remove and rename duplicates"
 #SBATCH --mail-user=pascal.amrein@unifr.ch
 #SBATCH --mail-type=begin,end,fail
@@ -26,14 +26,14 @@ echo "Current working directory: $(pwd)"
 
 
 
-# input_rename="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/all_original_files/"
-# output_rename="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/renamed_files/"
-# time python 01_renaming_compounds_and_reaction.py ${input_rename} ${output_rename} > info.out 2>&1 
+#input_rename="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/all_original_files/"
+#output_rename="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/renamed_files/"
+#time python 01_renaming_compounds_and_reaction.py ${input_rename} ${output_rename} > info.out 2>&1 
 
 
 input_duplicates="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/renamed_files/"
 output_duplicates="/home/pamrein/2024_masterthesis/read-MINE-results/read_mine_results/db_files/cleaned_files/"
-time python 02_remove_duplicates.py ${input_duplicates} ${output_duplicates} > info.out 2>&1 
+time python 02_remove_duplicates.py ${input_duplicates} ${output_duplicates} >> info.out 2>&1 
 
 # Deactivate the micromamba environment
 micromamba deactivate
